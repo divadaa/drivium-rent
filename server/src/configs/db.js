48b17b443f2cd9327f2b1f4016e45b65750fcf93
David Aguilar Alfayate <davidaguilaralfayate@MacBook-Pro-de-David.local>
2021-05-28 01:19:15 +0200

    css

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-// const DB_CAR = 'mongodb://localhost:27017/drivium';
-const DB_CAR = process.env.DB_URI;
+const DB_CAR = 'mongodb://localhost:27017/drivium';
+// const DB_CAR = process.env.DB_URI;
 
 mongoose
   .connect(DB_CAR, {
